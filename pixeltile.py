edb5d3b975b4d7064727559c107a4824eb65e288
Wisecrack <wisecrackdoesathing@protonmail.com>
2022-01-29 17:57:59 -0500

    load pixel maps of key and world map so we can manipulate individual pixels.

--- pixeltile.py
+++ pixeltile.py
@@ -4,7 +4,8 @@ h_key = Image.open("elevations.bmp")
 world_map = Image.open("biomes.bmp")
 #Now look through h_key, and if the pixel at [i][j] is NOT a 'water' pixel, then copy
 #the corresponding at pixel [i][j] in world_map, and save it at [i][j] into a new image
-
+hkey_px = h_key.load()
+world_px = world_map.load()
 
 #when we're done save the output image
 
