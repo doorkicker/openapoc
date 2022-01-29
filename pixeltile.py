94b0027a4ab1f3906f4699d8ec08c668f9f18940
Wisecrack <wisecrackdoesathing@protonmail.com>
2022-01-29 18:06:51 -0500

    create new image in pixeltile, and iterate over the image.

--- pixeltile.py
+++ pixeltile.py
@@ -2,10 +2,21 @@ from PIL import Image
 
 h_key = Image.open("elevations.bmp")
 world_map = Image.open("biomes.bmp")
+w, h = h_key.size
 #Now look through h_key, and if the pixel at [i][j] is NOT a 'water' pixel, then copy
 #the corresponding at pixel [i][j] in world_map, and save it at [i][j] into a new image
 hkey_px = h_key.load()
 world_px = world_map.load()
+img_out = Image.new("RGB", (w, h))
+
+i = 0
+j = 0
+while i < w:
+	j = 0
+	while j < h:
+
+		j = j + 1 #pass
+	i = i + 1
 
 #when we're done save the output image
 
