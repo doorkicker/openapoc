2a70441cc54315f1966811e5b23dce19a737f85a
Wisecrack <wisecrackdoesathing@protonmail.com>
2022-01-29 23:44:05 -0500

    fix iteration, fix getpixel pulling from loaded images, rather than Image objects.

--- pixeltile.py
+++ pixeltile.py
@@ -7,15 +7,18 @@ w, h = h_key.size
 #the corresponding at pixel [i][j] in world_map, and save it at [i][j] into a new image
 hkey_px = h_key.load()
 world_px = world_map.load()
-img_out = Image.new("RGB", (w, h))
+img_out = Image.new("RGB", (w, h), (100, 0, 100))
+img_px = img_out.load()
 
 i = 0
 j = 0
 while i < w:
 	j = 0
 	while j < h:
-
-		j = j + 1 #pass
+		#if hkey_px.getpixel([i, j]) != (232, 182, 98) and hkey_px.getpixel([i, j]) != (239, 219, 84):
+		if h_key.getpixel((i, j)) != (232, 182, 98) and h_key.getpixel((i, j)) != (239, 219, 84):
+			pass
+		j = j + 1
 	i = i + 1
 
 #when we're done save the output image
